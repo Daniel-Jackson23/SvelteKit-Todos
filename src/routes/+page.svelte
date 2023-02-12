56664ee14dd7aa53308218f9901ffd703e4b9cd7
Daniel-Jackson23 <danieljackson104@googlemail.com>
2023-02-12 16:28:43 +0000

    completed code

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import "../app.css"
+    import TodoForm from "../components/TodoForm.svelte";
+    import Todo from "../components/Todo.svelte";
+    import {todos} from "../stores/todoStores.js";
+</script>
+
+<div class="container mx-auto my-6 max-w-lg">
+    <h1 class="text-2xl font-bold text-center text-gray-800 md:text-3xl">My Todo List</h1>
+    <TodoForm/>
+    {#each $todos as todo}
+        <Todo todo={todo} index={todo.id}/>
+    {/each}
+</div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    darkMode: false,
+    theme: {
+        extend: {}
+    },
+    plugins: []
+};
